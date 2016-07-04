229ba2d96e6c4e2fe0efa923bcb9a02dba89a59a
TellH <570495627@qq.com>
2016-07-04 15:20:37 +0800

    用堆优化Dijkstra算法

--- src/Graph/ShortestPath/Dijkstra.java
+++ src/Graph/ShortestPath/Dijkstra.java
@@ -1,7 +1,8 @@
 package Graph.ShortestPath;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import Graph.MinimumSpanningTree.Prim;
+import Graph.Tree.MinHeap;
+
 import java.util.Queue;
 import java.util.Scanner;
 
@@ -21,15 +22,22 @@ public abstract class Dijkstra {
     int n;//顶点数
     int m;//边数
     int[][] e;//有各点间距离的邻接矩阵
-    static class Dis {
-        int vex;
+
+    static class Dis implements Comparable<Dis> {
+        int vexId;
         int len;
 
-        public Dis(int vex, int len) {
-            this.vex = vex;
+        public Dis(int vexId, int len) {
+            this.vexId = vexId;
             this.len = len;
         }
+
+        @Override
+        public int compareTo(Dis o) {
+            return this.len - o.len;
+        }
     }
+
     public Dijkstra() {
         Scanner in = new Scanner(System.in);
         n = in.nextInt();
@@ -60,55 +68,57 @@ public abstract class Dijkstra {
 
     public void run(int srcVertex) {
         dis = new int[n];//最短距离的松弛数组
-        book = new int[n];//标记顶点属于哪个集合Q or P
+        book = new int[n];//标记顶点属于哪个集合Q or P 如果用堆优化就不需要该数组了，在堆中顶点都是非最短路径
         disPath = new int[n];//用于回溯最短路径
-        queue = new PriorityQueue<>(new Comparator<Dis>() {
+
+        //顶点对应在堆中数组的下标，便于修改堆中的值
+        int[] posInHeap = new int[n];
+        //用于堆排序的点
+        MinHeap<Dis> heap = new MinHeap<Dis>(Dis.class) {
             @Override
-            public int compare(Dis o1, Dis o2) {
-                return o1.len - o2.len;
+            protected void swap(int i, int j, Dis[] h) {
+                super.swap(i, j, h);
+                //同步更新
+                int t = posInHeap[h[i].vexId];
+                posInHeap[h[i].vexId] = posInHeap[h[j].vexId];
+                posInHeap[h[j].vexId] = t;
             }
-        });//最小堆
+        };
         //初始化dis数组
         for (int i = 0; i < n; i++) {
             dis[i] = e[srcVertex][i];
             disPath[i] = srcVertex;
+            posInHeap[i] = heap.size() + 1;
+            heap.push(new Dis(i, dis[i]));
         }
         book[srcVertex] = 1;
+        heap.pop();//弹出源点
         //松弛n-1次即可
         for (int i = 0; i < n - 1; i++) {
             //在Q集合的所有顶点中选择一个离源点s最近的顶点u（dis[u]最小）
-            int v = findNearestVertex();
+            int v = heap.pop().vexId;//弹出已形成最短路径的顶点
             //加入到集合P
             book[v] = 1;
             //并以该点u为中转点，对每一条边进行松弛操作。
             for (int j = 0; j < n; j++) {
-                if (e[v][j] < INF && dis[j] > dis[v] + e[v][j]) {
+                if (e[v][j] < INF && book[j] == 0 && dis[j] > dis[v] + e[v][j]) {
                     dis[j] = dis[v] + e[v][j];
                     disPath[j] = v;
+                    heap.update(posInHeap[j], new Dis(j, dis[j]));//更新在堆中对应顶点的dis值
                 }
             }
         }
         onFinish(dis);
     }
 
-    abstract void onFinish(int[] dis) ;
-
-    private int findNearestVertex() {
-        queue.clear();
-        for (int i = 0; i < n; i++) {
-            if (book[i] == 0) {
-                queue.add(new Dis(i, dis[i]));
-            }
-        }
-        return queue.peek().vex;
-    }
+    abstract void onFinish(int[] dis);
 
     public static void main(String[] args) {
         Dijkstra d = new Dijkstra() {
             @Override
             void onFinish(int[] dis) {
                 for (int i = 0; i < n; i++) {
-                    System.out.print(dis[i]+" ");
+                    System.out.print(dis[i] + " ");
                 }
             }
         };
